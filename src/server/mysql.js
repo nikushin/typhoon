c1527bcbf9194b423401b3b8b214c85960b7e787
nikushin <57147367+nikushin@users.noreply.github.com>
2020-09-03 12:23:10 +0300

    sql1

--- src/server/mysql.js
+++ src/server/mysql.js
@@ -3,9 +3,24 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
-  database: "main",
   password: "83528352"
 });
 
+connection.query("CREATE DATABASE IF NOT EXISTS main", function (err, result) {
+  if (err) console.log(err);
+  connection.query("USE main");
+  connection.query("CREATE TABLE IF NOT EXISTS `parameters` (name CHAR(50), value TINYINT)", (err, results) => {
+    console.log(results);
+    if (results.warningStatus === 0) {
+      connection.query("INSERT parameters(name, value)\n" +
+        "VALUES ('temp_set_point', '0')");
+    }
+  });
+});
+
+
+
+
 
 module.exports = connection;
+
