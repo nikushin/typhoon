b54d2b9de86a2b8b6cf642d6aa32adae135f7c90
nikushin <nk@nxt.ru>
2021-03-03 19:31:39 +0300

    check

--- src/server/server.js
+++ src/server/server.js
@@ -5,16 +5,21 @@
   global.socket = require('socket.io')(8080, {transports: ['polling', 'websocket']} ).sockets;
   const EventEmitter = new require('events').EventEmitter;
   global.emitter = new EventEmitter();
+
+  await require('./equipment/equipment_init');
+  console.log('equipment_init');
+
+  await require('./phases/steps_init');
+  console.log('steps_init');
+
+  await require ('./function-bloks/emitt-socket')();
+  console.log('emitt-socket');
+
   if (process.platform === 'linux') {
     await require('./modbus/modbus')();
     require('./function-bloks/gpio')();
     require('./function-bloks/gpio2')();
   }
-  require('./equipment/equipment_init');
-  require('./phases/steps_init');
-
-  require ('./function-bloks/emitt-socket')();
-
   const {ioConnect} = require ('./function-bloks/io-connect');
   global.socket.on('connect', socket => {
     ioConnect(socket);
